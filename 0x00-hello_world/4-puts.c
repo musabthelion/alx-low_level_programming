fd9002e403a41920acd2ba73ba179e35092c42ae
musabthelion <musabthelion@gmail.com>
2023-03-16 16:16:04 -0700

    ans of 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*this is code to proithe crtain lines 
+ * using puts command 
+ * which is important
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
