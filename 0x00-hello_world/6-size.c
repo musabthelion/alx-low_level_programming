3e44a3c18d686956e48ad13985498e95d0d1b762
musabthelion <musabthelion@gmail.com>
2023-03-16 18:14:24 -0700

    ans

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,19 +16,11 @@ int main(void)
 	printf("Size of long int: %lu.\n",  (unsigned long int)sizeof (li));
 	printf("Size of long long int: %lu.\n",  (unsigned long long int)sizeof (lli));
 	printf("Size of float: %f.\n",  (float)sizeof (f));
-
-	 printf("Size of char: %ls.\n",  (signed char)sizeof (c));
-	         printf("Size of int: %ls.\n",  (signed int)sizeof (i));
-		         printf("Size of long int: %ls.\n",  (signed long int)sizeof (li));
-			         printf("Size of long long int: %ls.\n",  (signed long long int)sizeof (lli));
-				         printf("Size of float: %f.\n",  (float)sizeof (f));
+	printf("Size of char: %ls.\n",  (signed char)sizeof (c));
+	printf("Size of int: %ls.\n",  (signed int)sizeof (i));
+	printf("Size of long int: %ls.\n",  (signed long int)sizeof (li));
+	printf("Size of long long int: %ls.\n",  (signed long long int)sizeof (lli));
+	printf("Size of float: %f.\n",  (float)sizeof (f));
 
 	return (0);
-
-
-
-
-
-
-
 }
