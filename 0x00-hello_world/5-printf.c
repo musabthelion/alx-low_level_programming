e71934dac13ca8f1700c86450fb1409b96b8e3cd
musabthelion <musabthelion@gmail.com>
2023-03-16 17:27:30 -0700

    ans of 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*  main - entry point
-*  return :0
+*  main - Entry Point
+*  Return: 0
 */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
