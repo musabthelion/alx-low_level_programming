1488166f48bf49cc04e5e2e26381ab7094ac8e99
musabthelion <musabthelion@gmail.com>
2023-03-17 20:12:47 -0700

    ans

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - number combinations separted by comma
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+		if (ch == 9)
+			break;
+		putchar(',');
+		putchar (' ');
+	}
+	putchar('\n');
+	return (0);
+}
