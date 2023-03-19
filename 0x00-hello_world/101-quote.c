376b7b9a9145bfb86cec9e0b2d1a32213f0324d3
musabthelion <musabthelion@gmail.com>
2023-03-19 10:14:56 -0700

    ansdd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -print a string
+ * Return: 1
+ */
+
+int main(void)
+{
+	char p[] = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+		putchar (p);
+	return (1);
+}
