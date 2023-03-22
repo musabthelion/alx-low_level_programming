920896815226688962721cdc7cf5fa323b71f664
musabthelion <musabthelion@gmail.com>
2023-03-22 14:54:52 -0700

    ans

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ * _islower - function routione
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
