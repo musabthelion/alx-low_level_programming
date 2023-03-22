4988d854e676fc50596227e16b32cdb69a6d461f
musabthelion <musabthelion@gmail.com>
2023-03-22 16:39:34 -0700

    ans

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ * _abs - absolute function
+ * @int: is an integer
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int i;
+
+	if (i < 0)
+	{
+		i = -i;
+	}
+	return (0);
+}
