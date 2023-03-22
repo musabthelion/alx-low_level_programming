494eaf71392a8f5bd2baceecc6b9b92abc1b54e8
musabthelion <musabthelion@gmail.com>
2023-03-22 16:58:58 -0700

    ans

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ * print_last_digit - prints last digit
+ * @i: integer
+ * Return: value
+ */
+
+int print_last_digit(int i)
+{
+	int j;
+
+	j = (i % 10);
+	return (j);
+}
