01724d70b75b0416cba30fedbdd0155f3a5dd215
musabthelion <musabthelion@gmail.com>
2023-03-22 15:38:11 -0700

    ans

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_sign - function
+ * @n : sign of the muber
+ * Return: 1 and print +if Returun: 0 if neq Return: -1 if
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar ('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		putchar ('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		putchar ('-');
+		return (-1);
+	}
+}
