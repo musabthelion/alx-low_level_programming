6bde3c5cf6f92f596bebfaac6c24031b4af9e83a
musabthelion <musabthelion@gmail.com>
2023-03-22 07:10:05 -0700

    ans

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 27; i++)
+	{
+	print_alphabet();
+	}
+return (0);
+}
