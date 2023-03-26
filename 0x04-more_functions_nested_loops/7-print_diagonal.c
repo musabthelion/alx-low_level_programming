c68c044e02ad185d18ae42838823a3bc5e53caba
musabthelion <musabthelion@gmail.com>
2023-03-25 17:39:48 -0700

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,6 +3,7 @@
 
 /**
  * print_diagonal - prints diagonally
+ * @n: integer
  */
 
 void print_diagonal(int n)
@@ -10,7 +11,21 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		putchar('\n');
-	}
-	else
+	} else
 	{
-		for (i = 0;
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					putchar('\\');
+				else if (j < i)
+					putchar(' ');
+			}
+			putchar('\n');
+		}
+	}
+}
+
