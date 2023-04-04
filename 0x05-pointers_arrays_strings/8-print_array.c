21577f30a4cff788236c90c00a6ecb51524ed13d
musabthelion <musabthelion@gmail.com>
2023-04-04 09:36:04 -0700

    array not pointer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include"main.h"
+
+/**
+ * print_array - funciton called
+ *
+ * @a: pointer
+ * @n: integer
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if(i == n-1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+	putchar('\n');
+}
