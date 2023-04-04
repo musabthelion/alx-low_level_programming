e30ac758ce42da41a9ee048f7ec2982c6619fc16
musabthelion <musabthelion@gmail.com>
2023-04-04 03:36:52 -0700

    4 reverse print

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * main -check the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I love fto play table tennis";
+	print_rev(str);
+	return (0);
+}	
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+/**
+ * print_rev - function
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, size;
+
+	size = strlen(s);
+	for (i = size; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
