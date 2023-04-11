b888e3f29e00a0dc2993dbcb461575386fe70131
musabthelion <musabthelion@gmail.com>
2023-04-11 09:53:38 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ * main - function call
+ * @argv: vector
+ * @argc: count
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (i == 0)
+	{
+		printf("%s %d\n", argv[i], argc);
+	}
+	putchar('\n');
+	return (0);
+}
