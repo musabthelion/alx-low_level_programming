d39b2787f766b59574b024b1f25abd383c32bf6a
musabthelion <musabthelion@gmail.com>
2023-04-13 05:18:11 -0700

    task4 additoion

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+
+/**
+ * main- add arguments
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0, 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	if  (argc < 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 1; argv[i][j] != '\0';  j++)
+			{
+				if ((argv[i][j] >= '0') && (argv[i][j] <= '9'))
+				{
+					sum = (sum + (atoi(argv[i])));
+					printf("%d\n", sum);
+				}
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+	}
+	return (0);
+}
