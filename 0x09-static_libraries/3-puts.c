a413aa10cc403d9602d7a9ed5626d62ec82b3826
Musab Shabbir <musabthelion@gmail.com>
2023-04-27 14:13:51 -0700

    static librarirs

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _puts - function
+ *
+ * @str: pointer
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
